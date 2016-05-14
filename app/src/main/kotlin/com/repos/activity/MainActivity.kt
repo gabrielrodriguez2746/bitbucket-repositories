14d25e070b78a44a2524ae4a5d6dff40a3878b13
Gabriel <gabriel.rodriguez3103@gmail.com>
2016-05-14 03:02:19 -0300

    :lipstick: Add toolbar to Main Activity and set its naviagtion view

--- app/src/main/kotlin/com/repos/activity/MainActivity.kt
+++ app/src/main/kotlin/com/repos/activity/MainActivity.kt
@@ -2,11 +2,17 @@ package com.repos.activity
 
 import android.os.Bundle
 import com.repos.R
+import kotlinx.android.synthetic.main.toolbar_layout.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar()
+    }
+
+    fun setupToolbar() {
+        toolbar.setNavigationIcon(R.drawable.ic_menu)
     }
 }
