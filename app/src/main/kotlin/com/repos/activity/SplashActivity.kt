7369125d5024090ac237fda9cfb6701a1f77c837
Gabriel <gabriel.rodriguez3103@gmail.com>
2016-05-14 01:46:37 -0300

    :star: Add Splash Activity

---
+++ app/src/main/kotlin/com/repos/activity/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.repos.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.repos.bitbucketrepositories.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
