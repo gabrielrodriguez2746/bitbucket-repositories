92ca2cc139b3243b26a1a4a53a2a36ea73ca8375
Gabriel <gabriel.rodriguez3103@gmail.com>
2016-05-15 19:36:39 -0300

    :star: Get respositories response from web service

--- app/src/main/kotlin/com/repos/activity/MainActivity.kt
+++ app/src/main/kotlin/com/repos/activity/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.repos.activity
 
 import android.os.Bundle
-import android.view.Gravity
+import android.util.Log
 import com.repos.R
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.toolbar_layout.*
+import com.repos.RepositoriesApp
+import com.repos.listener.GitHubService
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val service = RepositoriesApp.instance!!.retrofit.create(GitHubService::class.java)
+        service.getRepositories().subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { response -> Log.d("MainActivity", "Response :: " + response.items) }
+
     }
 }
+
