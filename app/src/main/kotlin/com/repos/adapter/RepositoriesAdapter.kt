42c598032ac7f5655a1fa42374a2a99479d6ec7b
Gabriel <gabriel.rodriguez3103@gmail.com>
2016-05-15 23:50:38 -0300

    :star: Add Repositories Adapter

---
+++ app/src/main/kotlin/com/repos/adapter/RepositoriesAdapter.kt
@@ -0,0 +1,22 @@
+package com.repos.adapter
+
+import android.view.ViewGroup
+import com.repos.model.Repositories
+import com.repos.view.RepositoryItemView
+
+/**
+ * @author Gabriel Rodriguez
+ * @version 1.0
+ */
+class RepositoriesAdapter : RecyclerViewBaseAdapter<Repositories, RepositoryItemView>() {
+
+    override fun onCreateItemView(parent: ViewGroup, viewType: Int): RepositoryItemView {
+        return RepositoryItemView(parent.context)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewWrapper<RepositoryItemView>?, position: Int) {
+        val repository = items[position]
+        holder?.view?.bind(repository)
+    }
+}
