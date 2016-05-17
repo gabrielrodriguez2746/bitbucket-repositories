535197fc705436b481fbf796647bed9263efcccf
Gabriel <gabriel.rodriguez3103@gmail.com>
2016-05-16 22:47:51 -0300

    :star: Show loading while get the data from the web service

--- app/src/main/kotlin/com/repos/activity/MainActivity.kt
+++ app/src/main/kotlin/com/repos/activity/MainActivity.kt
@@ -5,8 +5,11 @@ import com.repos.R
 import com.repos.RepositoriesApp
 import com.repos.adapter.RepositoriesAdapter
 import com.repos.listener.GitHubService
+import com.repos.view.hide
 import com.repos.view.linearVertical
+import com.repos.view.show
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.circular_loading.*
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
@@ -21,16 +24,24 @@ class MainActivity : BaseActivity() {
         getRepositories()
     }
 
+    /**
+     * SetUp the [Repositories] [RecyclerView]
+     */
     fun setUpRepositoriesRecyclerView() {
         rv_repositories.linearVertical()
         rv_repositories.adapter = mRepositoriesAdapter
     }
 
+    /**
+     * Get [Repositories] from the web Service
+     */
     fun getRepositories() {
+        loading.show()
         val service = RepositoriesApp.instance!!.retrofit.create(GitHubService::class.java)
         service.getRepositories().subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe { response ->
+                    loading.hide()
                     mRepositoriesAdapter.items = response.items
                 }
     }
