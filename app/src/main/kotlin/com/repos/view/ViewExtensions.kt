0ac3bdcb66464746ba69fd7051216acd84657dfc
Gabriel <gabriel.rodriguez3103@gmail.com>
2016-05-16 22:47:26 -0300

    :star: Add extension method to show or hide views

--- app/src/main/kotlin/com/repos/view/ViewExtensions.kt
+++ app/src/main/kotlin/com/repos/view/ViewExtensions.kt
@@ -2,13 +2,31 @@ package com.repos.view
 
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
 
 /**
  * @author Gabriel Rodriguez
  * @version 1.0
  */
 
+/**
+ * Configure a [RecyclerView] As a Vertical
+ */
 fun RecyclerView.linearVertical() {
     layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
     setHasFixedSize(true)
+}
+
+/**
+ * Make a [View] Visible
+ */
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+/**
+ * Make a [View] Invisible
+ */
+fun View.hide() {
+    visibility = View.GONE
 }
