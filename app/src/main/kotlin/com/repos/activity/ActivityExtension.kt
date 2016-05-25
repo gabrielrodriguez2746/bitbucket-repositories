64b29c6068cc614dd476e5f9219841f77bda73a9
Gabriel <gabriel.rodriguez3103@gmail.com>
2016-05-25 20:24:38 -0300

    :star: Add function to add fragments to backstack with animation

--- app/src/main/kotlin/com/repos/activity/ActivityExtension.kt
+++ app/src/main/kotlin/com/repos/activity/ActivityExtension.kt
@@ -2,11 +2,16 @@ package com.repos.activity
 
 import android.app.Activity
 import android.content.Intent
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import com.repos.R
 
 /**
  * @author Gabriel Rodriguez
  * @version 1.0
  */
+
+val BACKSTACK = "backStack"
 
 /**
  * Launch [MainActivity] with the corresponding Flags to be a new Task
@@ -16,4 +21,15 @@ fun Activity.launchMainActivityAsNewTask() {
     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     this.startActivity(intent)
+}
+
+/**
+ * Add a Fragment to the container.
+ * @param fragment to add on the Back Stack
+ * @param containerId where the fragment will be place
+ */
+fun AppCompatActivity.addFragmentToBackStack(fragment: Fragment, containerId: Int, fragmentTag: String? = null) {
+    this.supportFragmentManager.beginTransaction().setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right,
+            R.anim.enter_from_right, R.anim.exit_to_right).add(containerId, fragment, fragmentTag ?: fragment.tag)
+            .addToBackStack(BACKSTACK).commit()
 }
